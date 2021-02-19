14e3f48143ea5ddba2984d62e4ad96352bc72ea6
Ryyap <ryyap@hotmail.com>
2021-02-19 17:53:11 +0800

    Update to Map,  MDFString and WayPointState
    Map
    -update MDF building for updateMDF()
    -added updateMDF2()
    
    MDFString
    -add mdfHex2 and mdfBinary2 variables, get method and set method
    -modify hexToBin() and binToHex() to be able to update mdfHex2 and mdfBinary2
    
    EventHandler
    -added code to update MDF and write to notepad

--- src/map/MDFString.java
+++ src/map/MDFString.java
@@ -5,6 +5,8 @@ import java.util.Locale;
 public class MDFString {
     String mdfHex="";
     String mdfBinary="";
+    String mdfHex2="";
+    String mdfBinary2="";
 
     public String getMDFHex(){
        return mdfHex;
@@ -12,8 +14,7 @@ public class MDFString {
 
     public void setMDFHex(String input){
         mdfHex = input;
-        System.out.println(mdfHex);
-        hexToBin();
+        hexToBin(mdfHex, 1);
    }
 
     public String getMDFBinary(){
@@ -22,14 +23,32 @@ public class MDFString {
 
     public void setMDFBinary(String input){
         mdfBinary = input;
-        binToHex();
+        binToHex(mdfBinary, 1);
     }
 
-    private void hexToBin(){
+    public String getMDFHex2(){
+        return mdfHex2;
+    }
+
+    public void setMDFHex2(String input){
+        mdfHex2 = input;
+        hexToBin(mdfHex2, 2);
+    }
+
+    public String getMDFBinary2(){
+        return mdfBinary2;
+    }
+
+    public void setMDFBinary2(String input){
+        mdfBinary2 = input;
+        binToHex(mdfBinary2, 2);
+    }
+
+    private void hexToBin(String mdfH, int num){
         String bin="";
         int j=0;
         for(int i=0;i<76;i++){
-            j = Integer.parseInt(mdfHex.substring(i,i+1), 16);
+            j = Integer.parseInt(mdfH.substring(i,i+1), 16);
             if(j<8){
                 for(int k=0; k<(4-Integer.toBinaryString(j).length()); k++){
                     bin+=0;
@@ -39,17 +58,27 @@ public class MDFString {
                 bin += Integer.toBinaryString(j);
             }
         }
-        mdfBinary = bin;
+        if(num==1){
+            mdfBinary = bin;
+        }else{
+            mdfBinary2 = bin;
+        }
+
     }
 
-    private void binToHex(){
+    private void binToHex(String mdfB, int num){
         String hexStr="";
         int decimal=0;
-        for(int i=0;i<304;i+=4){
-            decimal = Integer.parseInt(mdfBinary.substring(i,i+4),2);
+        for(int i=0;i< mdfB.length();i+=4){
+            decimal = Integer.parseInt(mdfB.substring(i,i+4),2);
             hexStr += Integer.toString(decimal, 16).toUpperCase(Locale.ROOT);
         }
-        mdfHex = hexStr;
+
+        if (num==1){
+            mdfHex = hexStr;
+        }else{
+            mdfHex2=hexStr;
+        }
     }
 
 }
--- src/map/WayPointState.java
+++ src/map/WayPointState.java
@@ -2,4 +2,7 @@ package map;
 
 public enum WayPointState {
     isObstacle, isEmpty, isUnexplored;
+    //isObstacle represents obstacle grid
+    //isEmpty represents explored grid
+    //isUnexplored represents unexplored grid
 }
